ae54a92b7ff781fb444c0e4d10dad243bbf5633d
shengjianfeng <jianfeng2612282@163.com>
2025-10-04 16:55:25 +0800

    feature: 测试

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default async function RootLayout({
   const locale = await getLocale();
   setRequestLocale(locale);
 
+
   const webUrl = process.env.NEXT_PUBLIC_WEB_URL || "";
   const googleAdsenseCode = process.env.NEXT_PUBLIC_GOOGLE_ADCODE || "";
 
