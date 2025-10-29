da18485d47eeee14c6a1a1ded92f36775a282cc9
SCUTBrothers <1260812658@qq.com>
2025-10-29 20:20:00 +0800

    chore: merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-import bundleAnalyzer from "@next/bundle-analyzer"
-import { createMDX } from "fumadocs-mdx/next"
-import createNextIntlPlugin from "next-intl/plugin"
-import { codeInspectorPlugin } from "code-inspector-plugin"
+import bundleAnalyzer from "@next/bundle-analyzer";
+import { createMDX } from "fumadocs-mdx/next";
+import createNextIntlPlugin from "next-intl/plugin";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
-const withMDX = createMDX()
+const withMDX = createMDX();
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
-})
+});
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
@@ -25,49 +25,33 @@ const nextConfig = {
     ],
   },
   async rewrites() {
-    const defaultOrigin = "https://validflow.airelief.cn"
-    const devOrigin = {
-      local: "http://127.0.0.1:8000",
-      development: "https://validflow.airelief.cn",
-    }
-
-    const origin = devOrigin[process.env.NODE_ENV] || defaultOrigin
-
     if (process.env.NODE_ENV === "production") {
-      return []
-    } else {
-      return [
-        {
-          source: "/api/copilotkit/:path*",
-          destination: "https://validflow.airelief.cn/api/copilotkit/:path*",
-        },
-        {
-          source: "/api/v1/:path*",
-          destination: `${origin}/api/v1/:path*`,
-        },
-      ];
+      return [];
     } else if (process.env.NODE_ENV === "test") {
       return [
         {
           source: "/api/v1/:path*",
           destination: "http://127.0.0.1:8000/api/v1/:path*",
-        }
+        },
       ];
     }
   },
+
   async redirects() {
-    return []
+    return [];
   },
   experimental: {
     turbo: {
       rules: codeInspectorPlugin({
         bundler: "turbopack",
         editor: "cursor",
-        dev: process.env.NODE_ENV === "local" || process.env.NODE_ENV === "development",
+        dev:
+          process.env.NODE_ENV === "local" ||
+          process.env.NODE_ENV === "development",
       }),
     },
   },
-}
+};
 
 // Make sure experimental mdx flag is enabled
 const configWithMDX = {
@@ -76,6 +60,6 @@ const configWithMDX = {
     ...nextConfig.experimental,
     mdxRs: true,
   },
-}
+};
 
-export default withBundleAnalyzer(withNextIntl(withMDX(configWithMDX)))
+export default withBundleAnalyzer(withNextIntl(withMDX(configWithMDX)));
